bbcae688447dc3d35616ed4344aa1974a96f92f4
Agacy junior <juniormartinssom@gmail.com>
2022-05-27 20:40:19 -0300

    Interface de agendamento

--- App.tsx
+++ App.tsx
@@ -14,8 +14,7 @@ import {
 } from "@expo-google-fonts/archivo";
 
 import theme from "./src/styles/theme";
-import { Home } from "./src/screens/Home";
-import { CarDetails } from "./src/screens/CarDetails";
+import { Scheduling } from "./src/screens/Scheduling";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -32,7 +31,7 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      <CarDetails />
+      <Scheduling />
     </ThemeProvider>
   );
 }
---
+++ src/screens/Scheduling/index.tsx
@@ -0,0 +1,57 @@
+import React from "react";
+import { BackButton } from "../../components/BackButton";
+
+import ArrowSvg from "../../assets/arrow.svg";
+import {
+  Container,
+  Content,
+  DateInfo,
+  DateTitle,
+  DateValue,
+  Footer,
+  Header,
+  RentalPeriod,
+  Title,
+} from "./styles";
+import { useTheme } from "styled-components";
+import { StatusBar } from "react-native";
+import { Button } from "../../components/Button";
+
+export function Scheduling() {
+  const theme = useTheme();
+  return (
+    <Container>
+      <StatusBar
+        barStyle="light-content"
+        translucent
+        backgroundColor="transparent"
+      />
+      <Header>
+        <BackButton onPress={() => {}} color={theme.colors.shape} />
+
+        <Title>
+          Escolha uma {"\n"}data de início e {"\n"}fim do aluguel
+        </Title>
+
+        <RentalPeriod>
+          <DateInfo>
+            <DateTitle>DE</DateTitle>
+            <DateValue selected={false}>28/06/2021</DateValue>
+          </DateInfo>
+
+          <ArrowSvg />
+          <DateInfo>
+            <DateTitle>ATÉ</DateTitle>
+            <DateValue selected={false}></DateValue>
+          </DateInfo>
+        </RentalPeriod>
+      </Header>
+
+      <Content></Content>
+
+      <Footer>
+        <Button title="Confirmar" />
+      </Footer>
+    </Container>
+  );
+}
