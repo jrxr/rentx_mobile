9a95af0714933ad90f3ac676b5ac2a19211b7582
Agacy junior <juniormartinssom@gmail.com>
2022-05-27 13:39:20 -0300

    Componente de Slider

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -3,29 +3,19 @@ import { StatusBar } from "react-native";
 import { RFValue } from "react-native-responsive-fontsize";
 
 import Logo from "../../assets/logo.svg";
-import { Container, Header, HeaderContent, TotalCars } from "./styles";
+import { Container, Header, HeaderContent, TotalCars, CarList } from "./styles";
 
-import { Car } from '../../components/Car';
+import { Car } from "../../components/Car";
 
 export function Home() {
   const carData = {
-    brand: 'Audi',
-    name: 'RS 5 Coupe',
+    brand: "Audi",
+    name: "RS 5 Coupe",
     rent: {
-      period: 'AO DIA',
+      period: "AO DIA",
       price: 120,
     },
-    thumbnail: 'https://freepngimg.com/thumb/audi/35227-5-audi-rs5-red.png'
-  };
-
-  const carDataTwo = {
-    brand: 'Audi',
-    name: 'A3',
-    rent: {
-      period: 'AO DIA',
-      price: 60,
-    },
-    thumbnail: 'https://freepngimg.com/thumb/audi/35227-5-audi-rs5-red.png'
+    thumbnail: "https://freepngimg.com/thumb/audi/35227-5-audi-rs5-red.png",
   };
 
   return (
@@ -42,8 +32,12 @@ export function Home() {
         </HeaderContent>
       </Header>
 
-      <Car data={carData} />
-      <Car data={carDataTwo} />
+      <CarList 
+        data={[1,2,3,4,5,6,7,8,9,10,11,12]}
+        keyExtractor={(item) => String(item)}
+        renderItem={({ item }) => <Car data={carData} />} 
+      />
+            
     </Container>
   );
 }
