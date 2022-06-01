37b16bd086b01267ec2ba18345a5f30cf6ccfea4
Agacy junior <juniormartinssom@gmail.com>
2022-05-31 21:06:07 -0300

    Primeira animação

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,43 +1,77 @@
 import React, { useEffect, useState } from "react";
 import { StatusBar } from "react-native";
+import { useNavigation } from "@react-navigation/native";
 import { RFValue } from "react-native-responsive-fontsize";
-import { Ionicons } from "@expo/vector-icons";
-import Logo from "../../assets/logo.svg";
-import { Container, Header, HeaderContent, TotalCars, CarList, MyCarsButton } from "./styles";
 
 import { api } from "../../services/api";
+
 import { Car } from "../../components/Car";
-import { useNavigation } from "@react-navigation/native";
-import { CarDTO } from "../../dtos/CarDTO";
-import { Load } from "../../components/Load";
-import { useTheme } from "styled-components";
+import { LoadAnimation } from "../../components/LoadAnimation";
+
+import Logo from "../../assets/logo.svg";
+
+import { Container, Header, HeaderContent, TotalCars, CarList } from "./styles";
 
 export function Home() {
-  const theme = useTheme();
-  const [cars, setCars] = useState<CarDTO[]>([]);
+  const [cars, setCars] = useState<ModelCar[]>([]);
   const [loading, setLoading] = useState(true);
+
+  const netInfo = useNetInfo();
   const navigation = useNavigation();
 
-  function handleCarDetails(car: CarDTO) {
+  function handleCarDetails(car: ModelCar) {
     navigation.navigate("CarDetails", { car });
   }
 
-  function handleOpenMyCars() {
-    navigation.navigate("MyCars");
+  async function offlineSynchronize() {
+    await synchronize({
+      database,
+      pullChanges: async ({ lastPulledAt }) => {
+        const response = await api.get(
+          `cars/sync/pull?lastPulledVersion=${lastPulledAt || 0}`
+        );
+
+        const { changes, latestVersion } = response.data;
+        return { changes, timestamp: latestVersion };
+      },
+      pushChanges: async ({ changes }) => {
+        const user = changes.users;
+        await api.post("/users/sync", user);
+      },
+    });
   }
 
   useEffect(() => {
+    let isMounted = true;
+
     async function fetchCars() {
       try {
-        const response = await api.get("/cars");
-        setCars(response.data);
+        const carCollection = await database.get<ModelCar>("cars");
+        const cars = await carCollection.query().fetch();
+
+        if (isMounted) {
+          setCars(cars);
+        }
       } catch (error) {
         console.log(error);
       } finally {
-        setLoading(false);
+        if (isMounted) {
+          setLoading(false);
+        }
       }
     }
+
+    fetchCars();
+    return () => {
+      isMounted = false;
+    };
   }, []);
+
+  useEffect(() => {
+    if (netInfo.isConnected === true) {
+      offlineSynchronize();
+    }
+  }, [netInfo.isConnected]);
 
   return (
     <Container>
@@ -49,24 +83,21 @@ export function Home() {
       <Header>
         <HeaderContent>
           <Logo width={RFValue(108)} height={RFValue(12)} />
-          <TotalCars>Total de {cars.length} carros</TotalCars>
+          {!loading && <TotalCars>Total de {cars.length} carros</TotalCars>}
         </HeaderContent>
       </Header>
+
       {loading ? (
-        <Load />
+        <LoadAnimation />
       ) : (
         <CarList
           data={cars}
-          keyExtractor={(item) => item.id}
+          keyExtractor={(item) => String(item.id)}
           renderItem={({ item }) => (
             <Car data={item} onPress={() => handleCarDetails(item)} />
           )}
         />
       )}
-
-      <MyCarsButton onPress={handleOpenMyCars}>
-        <Ionicons name="ios-car-sport" size={32} color={theme.colors.shape}  />
-      </MyCarsButton>
     </Container>
   );
 }
---
+++ src/screens/Splash/index.tsx
@@ -0,0 +1,87 @@
+import { useNavigation } from '@react-navigation/native';
+import React, { useEffect } from 'react';
+import { StatusBar } from 'react-native';
+import Animated, { 
+  useSharedValue, 
+  useAnimatedStyle,
+  withTiming,
+  interpolate,
+  Extrapolate,
+  runOnJS
+} from 'react-native-reanimated';
+
+import BrandSvg from '../../assets/brand.svg';
+import LogoSvg from '../../assets/logo.svg';
+
+import {
+  Container
+} from './styles';
+
+export function Splash() {
+  const splashAnimation = useSharedValue(0);
+
+  const navigation = useNavigation();
+
+  const brandStyle = useAnimatedStyle(() => {
+    return {
+      transform: [
+        {
+          translateX: interpolate(splashAnimation.value,
+            [0, 50],
+            [0, + 74],
+            Extrapolate.CLAMP  
+          )
+        },
+        {
+          scale: interpolate(splashAnimation.value,
+            [0, 50],
+            [1, .4],
+            Extrapolate.CLAMP          
+          )
+        }
+      ]
+    }
+  });
+
+  const logoStyle = useAnimatedStyle(() => {
+    return {
+      opacity: interpolate(splashAnimation.value, 
+        [0, 50, 100], 
+        [0, 0, 1]
+      ),
+    }
+  });
+
+  function startApp() {
+    navigation.navigate('SignIn');
+  }
+
+  useEffect(() => {
+    splashAnimation.value = withTiming(
+      100, 
+      { duration : 1500 },
+      () => {
+        'worklet'
+        runOnJS(startApp)()
+      }
+    );
+  },[]);
+   
+  return (
+    <Container>
+      <StatusBar 
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      <Animated.View style={[brandStyle, { position: 'absolute' }]}>
+        <BrandSvg width={80} height={50} />
+      </Animated.View>
+
+      <Animated.View style={[logoStyle, { position: 'absolute' }]}>
+        <LogoSvg width={180} height={20} />
+      </Animated.View>
+
+    </Container>
+  );
+}
